16b00467058b4a655d153b94887816f52b66acc3
Kbza <g.leonel.m17@gmail.com>
2024-08-18 21:53:05 -0300

    feat: nav, first routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { StoreComponent } from './components/pages/store/store.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    StoreComponent
   ],
   imports: [
     BrowserModule,
